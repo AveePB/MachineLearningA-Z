9feb5acc23566db596d05cd669dbe1a4772b8125
AveePB <mike303.py@gmail.com>
2024-06-13 23:18:57 +0200

    Built mlr model in python

---
+++ 0.models/regression/multiple-linear-regression.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class MultipleLinearRegression:
+
+    def __init__(self, lr = 0.001, n_iters = 1000):
+        self.__lr = lr
+        self.__n_iters = n_iters
+
+        self._coeff = None
+        self._inter = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self._coeff = np.zeros(n_features)
+        self._inter = 0
+
+        for _ in range(self.__n_iters):
+            y_pred = np.dot(X, self._coeff) + self._inter
+            print(y_pred)
+
+            dc = (1/n_samples) * np.dot(X.T, (y_pred - y))
+            di = (1/n_samples) * np.sum(y_pred - y)
+
+            self._coeff = self._coeff - self.__lr * dc
+            self._inter = self._inter - self.__lr * di
+
+    def predict(self, X: np.ndarray):
+        return np.dot(X, self._coeff) + self._inter
